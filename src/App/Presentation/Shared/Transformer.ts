b57744ab79e1bace184302058e45accc99c87db2
Roxnny A Barrios C <roxnny.barrios@gmail.com>
2021-04-08 21:16:05 -0400

    add new methods to Transformer.ts

--- src/App/Presentation/Shared/Transformer.ts
+++ src/App/Presentation/Shared/Transformer.ts
@@ -1,9 +1,13 @@
+import _ from "lodash";
+import moment from "moment";
+
 export default abstract class Transformer
 {
     abstract transform(data: any): any;
+
     handle(data: any): any
     {
-        let result = null;
+        let result;
 
         if (data instanceof Array)
         {
@@ -18,6 +22,22 @@ export default abstract class Transformer
         }
 
         return result;
+    }
+
+    validate(value: any, transformer: string = null): any
+    {
+        if(transformer)
+        {
+            // @ts-ignore
+            return !_.isNaN(value) && !_.isUndefined(value) ? this[transformer].handle(value) : null;
+        }
+
+        return !_.isNaN(value) && !_.isUndefined(value) ? value : null;
+    }
+
+    transformDate(date: Date): number
+    {
+        return  !_.isNaN(date) && !_.isUndefined(date) ? moment(date).utc().unix() : null;
     }
 }
 
