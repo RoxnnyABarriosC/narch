7d141e1a87310ecaffa7e687b93a29090e9d2c8c
Roxnny A Barrios C <roxnny.barrios@gmail.com>
2021-04-08 22:23:23 -0400

    add new methods to Filter.ts

--- src/App/InterfaceAdapters/Shared/IFilter.ts
+++ src/App/InterfaceAdapters/Shared/IFilter.ts
@@ -1,4 +1,31 @@
-import { SelectQueryBuilder } from "typeorm";
+import { SelectQueryBuilder } from "typeorm/query-builder/SelectQueryBuilder";
+
+export declare type AttributeConfig = {
+    attribute: string,
+    dbAttribute?: string,
+    isBoolean?: boolean ,
+    toLower?: boolean
+};
+
+export declare type SearchConfig = {
+    attributesDB: string | string[] | AttributeDBConfig[]
+    partialMatch?: boolean,
+}
+
+export declare type AttributeDBConfig = {
+    tableAlias?: string,
+    name: string,
+    coalesce?: boolean,
+    setweight?: SetweightRelevance,
+}
+
+export declare type SetweightRelevance = 'A' |  'B' |'C' |'D' ;
+
+export declare type FilterCondition = 'andWhere' | 'orWhere' | 'where';
+
+export declare type FilterOperator = '=' | 'ilike' | '>' | '>=' | '<=' | 'in';
+
+export declare type MultiFilterOperator = '=' | 'ilike';
 
 export default interface IFilter
 {
@@ -8,4 +35,10 @@ export default interface IFilter
     has(key: string): boolean;
     isEmpty(): boolean;
     getFields(): any[];
+    createFilter(queryBuilder: SelectQueryBuilder<any>, entityFilter: any, attributeConfig: string | AttributeConfig, condition: FilterCondition, operator: FilterOperator, alias?: string): void;
+    createSearchVector(queryBuilder: SelectQueryBuilder<any>, entityFilter: any, attribute: string , searchConfig: SearchConfig, condition: FilterCondition, alias?: string): void;
+    createSearchLike(queryBuilder: SelectQueryBuilder<any>, entityFilter: any, attribute: string , searchConfig: SearchConfig, condition: FilterCondition, alias?: string): void;
+    createMultiFilter(queryBuilder: SelectQueryBuilder<any>, entityFilter: any, attributeConfig: string | AttributeConfig, condition: FilterCondition, operator: MultiFilterOperator, alias?: string): void;
+    createBooleanMultiFilter(queryBuilder: SelectQueryBuilder<any>, entityFilter: any, attribute: string, value: boolean, condition?: FilterCondition, alias?: string): void;
+    getMultiFilter(entityFilter: any, attribute: string): string[];
 }
--- src/App/Presentation/Shared/Filter.ts
+++ src/App/Presentation/Shared/Filter.ts
@@ -1,5 +1,13 @@
 import {Request} from "express";
-import IFilter from "../../InterfaceAdapters/Shared/IFilter";
+import IFilter, {
+    AttributeConfig, AttributeDBConfig,
+    FilterCondition,
+    FilterOperator,
+    MultiFilterOperator, SearchConfig, SetweightRelevance
+} from "../../InterfaceAdapters/Shared/IFilter";
+import {SelectQueryBuilder} from "typeorm/query-builder/SelectQueryBuilder";
+import _ from "lodash";
+import {Brackets} from "typeorm";
 
 export default  abstract class Filter implements IFilter
 {
@@ -62,6 +70,287 @@ export default  abstract class Filter implements IFilter
     values(): Map<string, any>
     {
         return this.filters;
+    }
+
+    createFilter(queryBuilder: SelectQueryBuilder<any>, entityFilter: any, attributeConfig: string | AttributeConfig, condition: FilterCondition, operator: FilterOperator, alias: string = 'i'): void
+    {
+        let attribute: string = attributeConfig as string;
+        let dbAttribute: string;
+        let booleanAttribute: boolean = false;
+        let toLower: boolean = false
+
+        if (_.isObject(attributeConfig))
+        {
+            attribute = attributeConfig.attribute;
+
+            if (attributeConfig.dbAttribute)
+            {
+                dbAttribute = attributeConfig.dbAttribute;
+            }
+
+            if (attributeConfig.isBoolean)
+            {
+                booleanAttribute = attributeConfig.isBoolean;
+            }
+
+            if (attributeConfig.toLower)
+            {
+                toLower = attributeConfig.toLower;
+            }
+        }
+
+        if (_.isString(attributeConfig) || _.isUndefined(dbAttribute))
+        {
+            dbAttribute = `${entityFilter[attribute]}`;
+        }
+
+        if (this.has(entityFilter[attribute]))
+        {
+            let valueAttr: string | string[] | boolean = this.get(entityFilter[attribute]).trim();
+            let aliasAttr: string = `:${entityFilter[attribute]}`;
+
+            if (booleanAttribute)
+            {
+                valueAttr = valueAttr !== 'false';
+            }
+
+            if (operator === 'in')
+            {
+                aliasAttr = `(:...${entityFilter[attribute]})`;
+                valueAttr = this.getMultiFilter(entityFilter, attribute);
+            }
+
+            if (operator === 'ilike')
+            {
+                aliasAttr = `:${entityFilter[attribute]}`;
+                valueAttr = `%${valueAttr}%`;
+            }
+
+            if (toLower)
+            {
+                queryBuilder[condition](`LOWER(${alias}.${dbAttribute}) ${operator} LOWER(${aliasAttr})`);
+            }
+
+            else
+            {
+                queryBuilder[condition](`${alias}.${dbAttribute} ${operator} ${aliasAttr}`);
+
+            }
+
+            queryBuilder.setParameter(entityFilter[attribute], valueAttr);
+        }
+    }
+
+    getMultiFilter(entityFilter: any, attribute: string): string[]
+    {
+        let filters: string[] = [];
+
+        if (this.has(entityFilter[attribute]))
+        {
+            this.get(entityFilter[attribute]).trim().split(',').map(attr => {
+                if (attr.trim().length > 0) {
+                    filters.push(attr.trim());
+                }
+            });
+        }
+
+        return filters;
+    }
+
+    createBooleanMultiFilter(queryBuilder: SelectQueryBuilder<any>, entityFilter: any, attribute: string, value: boolean = true, condition: FilterCondition = 'andWhere', alias: string = 'i'): void
+    {
+        if (this.has(entityFilter[attribute]))
+        {
+            this.get(entityFilter[attribute]).trim().split(',').map((attr: string, index: number) => {
+                const where: string = index === 0 ? condition : `orWhere`;
+                if (attr.trim().length > 0)
+                {
+                    // @ts-ignore
+                    queryBuilder[where](`${alias}.${attr.trim()} = :value`);
+                    queryBuilder.setParameter('value', value);
+                }
+            });
+        }
+    }
+
+    createMultiFilter(queryBuilder: SelectQueryBuilder<any>, entityFilter: any, attributeConfig: string | AttributeConfig, condition: FilterCondition, operator: MultiFilterOperator, alias: string = 'i'): void
+    {
+        let attribute: string = attributeConfig as string;
+        let dbAttribute: string;
+        let toLower: boolean = false
+
+        if (_.isObject(attributeConfig))
+        {
+            attribute = attributeConfig.attribute;
+
+            if (attributeConfig.dbAttribute)
+            {
+                dbAttribute = attributeConfig.dbAttribute;
+            }
+
+            if (attributeConfig.toLower)
+            {
+                toLower = attributeConfig.toLower;
+            }
+        }
+
+        if (_.isString(attributeConfig) || _.isUndefined(dbAttribute))
+        {
+            dbAttribute = `${entityFilter[attribute]}`;
+        }
+
+        if (this.has(entityFilter[attribute]))
+        {
+            queryBuilder[condition]( new Brackets( qb => {
+                this.get(entityFilter[attribute]).trim().split(',').map((attr: string, index: number) => {
+                    const where: string = index === 0 ? `where` : `orWhere`;
+
+                    let aliasAttr: string = `${entityFilter[attribute]}_${index}`;
+                    let valueAttr: string = attr.trim();
+
+                    if (operator === 'ilike')
+                    {
+                        valueAttr = `%${valueAttr}%`;
+                    }
+
+                    if (valueAttr.length > 0)
+                    {
+                        if (toLower)
+                        {
+                            // @ts-ignore
+                            qb[where](`LOWER(${alias}.${dbAttribute}) ${operator} LOWER(:${aliasAttr})`, {[aliasAttr]: valueAttr});
+                        }
+
+                        else
+                        {
+                            // @ts-ignore
+                            qb[where](`${alias}.${dbAttribute} ${operator} :${aliasAttr}`, {[aliasAttr]: valueAttr});
+                        }
+                    }
+                });
+            }));
+        }
+    }
+
+    createSearchVector(queryBuilder: SelectQueryBuilder<any>, entityFilter: any, attribute: string , searchConfig: SearchConfig, condition: FilterCondition, alias: string = 'i'): void
+    {
+        const aliasAttr = `:${entityFilter[attribute]}`;
+
+        const partialMatch: boolean =  _.isUndefined(searchConfig?.partialMatch) ? true : searchConfig.partialMatch;
+
+        let attrsDB: string | string[] | AttributeDBConfig[] = searchConfig.attributesDB;
+
+        let searchAtt: string | string[];
+
+        if(typeof attrsDB === 'string')
+        {
+            searchAtt = `to_tsvector(${alias}.${attrsDB})`;
+        }
+
+        if (typeof attrsDB === 'object')
+        {
+            if (typeof attrsDB[0] === "string")
+            {
+                // @ts-ignore
+                searchAtt = attrsDB.map((attrDB: string) => `to_tsvector(${alias}.${attrDB})`).join(' || ');
+            }
+
+            if (typeof attrsDB[0] === "object")
+            {
+                // @ts-ignore
+                searchAtt = attrsDB.map((attrDB: AttributeDBConfig ) => {
+                    const coalesce: boolean = _.isUndefined(attrDB?.coalesce) ? false : attrDB.coalesce;
+                    const setweight: boolean | SetweightRelevance = _.isUndefined(attrDB?.setweight) ? false : attrDB.setweight;
+                    const tableAlias: string = _.isUndefined(attrDB?.tableAlias) ? alias : attrDB.tableAlias;
+
+                    const attr: string = `${tableAlias}.${attrDB.name}`;
+
+                    let attrDBSearch: string = `to_tsvector(${attr})`;
+
+                    if (coalesce)
+                    {
+                        attrDBSearch = attrDBSearch.replace(attr, `coalesce(${attr},'')`);
+                    }
+
+                    if (setweight)
+                    {
+                        attrDBSearch = `setweight(${attrDBSearch}, '${setweight}')`;
+                    }
+
+                    return attrDBSearch;
+                }).join(' || ');
+            }
+        }
+
+        if (this.has(entityFilter[attribute]))
+        {
+            let valueAttr: string | string[] = this.get(entityFilter[attribute]).trim();
+
+            if (valueAttr.length > 0)
+            {
+                if(partialMatch)
+                {
+                    valueAttr = valueAttr.split(' ').map((value) => `${value}:*`).join(' | ');
+                }
+
+                queryBuilder[condition](`${searchAtt} @@ to_tsquery(${aliasAttr})`);
+                queryBuilder.setParameter(entityFilter[attribute], valueAttr);
+            }
+        }
+    }
+
+    createSearchLike(queryBuilder: SelectQueryBuilder<any>, entityFilter: any, attribute: string , searchConfig: SearchConfig, condition: FilterCondition, alias: string = 'i'): void
+    {
+        const aliasAttr = `:${entityFilter[attribute]}`;
+
+        let attrsDB: string | string[] | AttributeDBConfig[] = searchConfig.attributesDB;
+
+        let searchAtt: string | string[];
+
+        if(typeof attrsDB === 'string')
+        {
+            searchAtt = `${alias}.${attrsDB}`;
+        }
+
+        if (typeof attrsDB === 'object')
+        {
+            if (typeof attrsDB[0] === "string")
+            {
+                // @ts-ignore
+                searchAtt = attrsDB.map((attrDB: string) => `${alias}.${attrDB}`).join(' || ');
+            }
+
+            if (typeof attrsDB[0] === "object")
+            {
+                // @ts-ignore
+                searchAtt = attrsDB.map((attrDB: AttributeDBConfig ) => {
+                    const coalesce: boolean = _.isUndefined(attrDB?.coalesce) ? false : attrDB.coalesce;
+                    const tableAlias: string = _.isUndefined(attrDB?.tableAlias) ? alias : attrDB.tableAlias;
+
+                    const attr: string = `${tableAlias}.${attrDB.name}`;
+
+                    let attrDBSearch: string = `${attr}`;
+
+                    if (coalesce)
+                    {
+                        attrDBSearch = attrDBSearch.replace(attr, `coalesce(${attr},'')`);
+                    }
+
+                    return attrDBSearch;
+                }).join(' || ');
+            }
+        }
+
+        if (this.has(entityFilter[attribute]))
+        {
+            let valueAttr: string | string[] = this.get(entityFilter[attribute]).trim().split(' ').map((attr) => `%${attr}%`).join(' ');
+
+            if (valueAttr.length > 0)
+            {
+                queryBuilder[condition](`${searchAtt} ILIKE ${aliasAttr}`);
+                queryBuilder.setParameter(entityFilter[attribute], valueAttr);
+            }
+        }
     }
 
     abstract getFields(): any[];
