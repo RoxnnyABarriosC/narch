78b31c41056f5bb1318fc0e1a8a0825759f19e36
Roxnny A Barrios C <roxnny.barrios@gmail.com>
2021-04-13 13:09:54 -0400

    add log transformer

---
+++ src/Log/Presentation/Transformers/Log.transformer.ts
@@ -0,0 +1,29 @@
+import Transformer from "../../../App/Presentation/Shared/Transformer";
+import ILogDomain from "../../InterfaceAdapters/ILog.domain";
+import UserRelationshipTransformer from "../../../User/Presentation/Transformers/UserRelationship.transformer";
+
+export default class LogTransformer extends Transformer
+{
+    private userRelationshipTransformer: UserRelationshipTransformer;
+
+    constructor()
+    {
+        super();
+        this.userRelationshipTransformer = new UserRelationshipTransformer();
+    }
+
+    public transform(log: ILogDomain)
+    {
+        return {
+            id: log.getId(),
+            action: log.action,
+            entity: log.entity,
+            entityId: log.entityId,
+            description: log.description,
+            metadata: log.metadata,
+            createdBy: this.validate(log.getCreatedBy(),'userRelationshipTransformer'),
+            createdAt: this.transformDate(log.createdAt),
+            updatedAt: this.transformDate(log.updatedAt),
+        };
+    }
+}
