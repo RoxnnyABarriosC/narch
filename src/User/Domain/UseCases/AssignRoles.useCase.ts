57fd24ab59f731566bfb0676ee1e091623537942
Roxnny A Barrios C <roxnny.barrios@gmail.com>
2021-04-24 10:22:26 -0400

    fix in AssingRoles use cases

--- src/User/Domain/UseCases/AssignRoles.useCase.ts
+++ src/User/Domain/UseCases/AssignRoles.useCase.ts
@@ -2,7 +2,7 @@ import lazyInject from "../../../LazyInject";
 import {REPOSITORIES} from "../../../Repositories";
 import IUserRepository from "../../InterfaceAdapters/IUser.repository";
 import IRoleRepository from "../../../Role/InterfaceAdapters/IRole.repository";
-import UserAssignRolePayload from "../../InterfaceAdapters/Payloads/UserAssignRole.payload";
+import UserAssignRolesPayload from "../../InterfaceAdapters/Payloads/UserAssignRoles.payload";
 import IUserDomain from "../../InterfaceAdapters/IUser.domain";
 import IRoleDomain from "../../../Role/InterfaceAdapters/IRole.domain";
 import _ from "lodash";
@@ -17,7 +17,7 @@ export default class AssignRolesUseCase
     @lazyInject(REPOSITORIES.IRoleRepository)
     private roleRepository: IRoleRepository<IRoleDomain>;
 
-    async handle(payload: UserAssignRolePayload): Promise<IUserDomain>
+    async handle(payload: UserAssignRolesPayload): Promise<IUserDomain>
     {
         let user: IUserDomain = await this.repository.getOne(payload.getId());
         const oldUser: IUserDomain = _.cloneDeep<IUserDomain>(user);
